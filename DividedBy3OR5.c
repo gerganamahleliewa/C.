5e1588c2851e70283afdc1962d82cff7be995dee
Gergana Mahlelieva <116373050+gerganamahleliewa@users.noreply.github.com>
2023-06-14 15:19:46 +0300

    Create DividedBy3OR5.c

---
+++ DividedBy3OR5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+
+
+   int num,sum=0;
+   printf("Enter number:");
+
+   scanf("%d",&num);
+
+for(int i=1;i<=num;++i)
+{
+    if(i%3==0 || i%5==0)
+    {
+        sum+=i;
+    }
+}
+printf("Sum=%d",sum);
+    return 0;
+}
