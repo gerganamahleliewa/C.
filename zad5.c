f3b281ff322b125b05b9e19ab00b48bca75b6d7a
Gergana Mahlelieva <116373050+gerganamahleliewa@users.noreply.github.com>
2023-06-15 09:59:59 +0300

    Create zad5.c

---
+++ zad5.c
@@ -0,0 +1,14 @@
+Hex = 0xC3                 Hex=618                      Hex= 30                        Hex=30
+binary = 0b1100 0011       binary=0b011000011000        binary= 0b000000110000         binary= 0b000000110000
+  
+  
+Hex = 0x75                  Hex=3A8                    Hex=1D                          Hex=1D
+binary = 0b01110101         binary=0b001110101000      binary=0b000000011101           binary=0b000000011101
+  
+  
+Hex = 0x87                  Hex=438                     Hex=21                          Hex=21
+binary =0b10000111          binary=0b010000111000       binary=0b000000100001           binary=0b000000100001 
+  
+  
+Hex = 0x66                  Hex=330                     Hex=19                          Hex=19
+binary = 0b01100110         binary=0b001100110000       binary=0b000000011001           binary=0b000000011001
