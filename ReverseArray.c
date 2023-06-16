27e37624bf656ce0c73aacbf63f6ddf3b5d17093
Gergana Mahlelieva <116373050+gerganamahleliewa@users.noreply.github.com>
2023-06-16 09:59:07 +0300

    Create ReverseArray.c

---
+++ ReverseArray.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+#include <ctype.h>
+
+
+
+int main() {
+
+//create array
+    int arr[5]={1,5,6,9,7};
+
+  //print  reverse array 
+    for(int i = 4; i >=0 ; --i)
+    {
+        printf("%d ",arr[i]);
+    }
+
+    return 0;
+
+}
