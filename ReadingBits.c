294c54394b2af5f5999d4030087a64fe343c3a8e
Gergana Mahlelieva <116373050+gerganamahleliewa@users.noreply.github.com>
2023-06-30 18:06:44 +0300

    Update ReadingBits.c

--- ReadingBits.c
+++ ReadingBits.c
@@ -18,8 +18,10 @@ int main() {
     printf("Enter bit to check:");
     scanf("%d",&position);
 
+    //left shift 1 ,n times
     newNum = ( 1 << position) | num;
-
+    
+  //print number in decimal before and after
     printf("Number before %d bit: %d in decimal\n",position,num);
     printf("Number after %d bit: %d in decimal",position,newNum);
 
